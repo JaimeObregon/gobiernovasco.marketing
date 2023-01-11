85d7ce67834cd600e1bfca7fd6f853c38a706dc4
Jaime Gómez-Obregón <jaime@gomezobregon.com>
2023-01-11 22:19:31 +0100

    Sigue desplegando el andamiaje del proyecto

--- bin/build.sh
+++ bin/build.sh
@@ -2,4 +2,7 @@
 
 yarn install || exit 1
 bin/build.js || exit 2
-sed -i '' -E "s/\/(app\.(css|js))/\/build\/\1/" httpdocs/index.html
+
+# Esta es la semántica de sed para el entorno Linux de Netlify.
+# La semántica de sed en macOS es diferente.
+sed -E "s/\/(app\.(css|js))/\/build\/\1/" -i httpdocs/index.html
