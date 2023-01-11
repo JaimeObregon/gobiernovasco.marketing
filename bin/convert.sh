bdde56be4a7e90670786471d2c95dad58aa77d39
Jaime Gómez-Obregón <jaime@gomezobregon.com>
2023-01-11 23:48:09 +0100

    Sigue desplegando el andamiaje del proyecto

--- bin/convert.sh
+++ bin/convert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-for file in pdf/*pdf; do
-  pdftohtml -noframes -c -i -hidden $file || error 1
+
+for file in pdf/*.pdf; do
+  pdftohtml -noframes -c -i -p -hidden -enc UTF-8 $file
 done
 
 mv pdf/*html converted
