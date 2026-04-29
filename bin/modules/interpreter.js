412e930f8d03c7af7be4109ceb690e6039630e46
Jaime Gómez-Obregón <417226+JaimeObregon@users.noreply.github.com>
2026-04-29 19:29:08 +0200

    Actualiza el formato de un fichero

--- bin/modules/interpreter.js
+++ bin/modules/interpreter.js
@@ -19,20 +19,20 @@ class Interpreter {
     // campañas con un nombre ambiguo que se confunde con una _keyword_.
     this.html = this.html.replaceAll(
       /(CAMPAÑA|ANUNCIO OFICIAL)<br>\nNombre<br>\nANUNCIOS OFICIALES<br>\n/g,
-      '$1<br>\nNombre<br>\nANUNCIOS OFICIALES VARIOS<br>\n'
+      '$1<br>\nNombre<br>\nANUNCIOS OFICIALES VARIOS<br>\n',
     )
 
     // Lo mismo sucede aquí, en el PDF de 2021:
     this.html = this.html.replaceAll(
       /\nIRAGARKI OFIZIALA<br>\nIzena<br>\nIRAGARKI OFIZIALAK<br>\nHelburua<br>/g,
-      '\nIRAGARKI OFIZIALA<br>\nIzena<br>\nIragarki Ofizialak<br>\nHelburua<br>'
+      '\nIRAGARKI OFIZIALA<br>\nIzena<br>\nIragarki Ofizialak<br>\nHelburua<br>',
     )
 
     // A «Los 40» a veces los llaman simplemente «40». Lo arreglamos aquí,
     // para que el intérprete no lo confunda con cuarenta euros.
     this.html = this.html.replaceAll(
       /\n40(<br>\n[\d\.,]+€<br>\n)/g,
-      '\nLos 40$1'
+      '\nLos 40$1',
     )
 
     if (year === 2018) {
@@ -40,11 +40,11 @@ class Interpreter {
       // valores de los campos «Inversión total» y «Medio».
       this.html = this.html.replaceAll(
         /Inversión<br>\nRadio Popular-Herri Irratia<br>\nTOTAL<br>\nMedio<br>\n850,00 €<br>\n/g,
-        'Inversión<br>\n850,00 €<br>\nTOTAL<br>\nMedio<br>\nRadio Popular-Herri Irratia<br>\n'
+        'Inversión<br>\n850,00 €<br>\nTOTAL<br>\nMedio<br>\nRadio Popular-Herri Irratia<br>\n',
       )
       this.html = this.html.replaceAll(
         /Inversión<br>\nRadio Euskadi<br>\nTOTAL<br>\nMedio<br>\n4.367,00 €<br>\n/g,
-        'Inversión<br>\n4.367,00 €<br>\nTOTAL<br>\nMedio<br>\nRadio Euskadi<br>\n'
+        'Inversión<br>\n4.367,00 €<br>\nTOTAL<br>\nMedio<br>\nRadio Euskadi<br>\n',
       )
     } else if (year === 2019) {
       this.html = this.html.replaceAll(/137.013, 00€<br>/g, '137.013,00€<br>')
@@ -52,33 +52,33 @@ class Interpreter {
       // Corrige un caso en el que están intercambiados los valores de dos campos.
       this.html = this.html.replaceAll(
         /Soportes<br>\n41\.322,00€<br>\nInversión TOTAL<br>\nRadio y TV<br>\n/g,
-        'Soportes<br>\nRadio y TV<br>\nInversión TOTAL<br>\n41.322,00 €<br>\n'
+        'Soportes<br>\nRadio y TV<br>\nInversión TOTAL<br>\n41.322,00 €<br>\n',
       )
     } else if (year === 2020) {
       this.html = this.html.replaceAll(
         /Propaga<br>\n3.000,00 € impresión carteles<br>\n2.000,00 € banners y gestión online<br>/g,
-        'Propaga (impresión carteles)<br>\n3.000,00 €<br>\n<br>Propaga (banners y gestión online)<br>\n2.000,00 €<br>\n'
+        'Propaga (impresión carteles)<br>\n3.000,00 €<br>\n<br>Propaga (banners y gestión online)<br>\n2.000,00 €<br>\n',
       )
 
       this.html = this.html.replaceAll(
         /\nTotal<br>\n157.182,15€<br>/g,
-        '\nTOTAL<br>\n157.182,15€<br>'
+        '\nTOTAL<br>\n157.182,15€<br>',
       )
     } else if (year === 2021) {
       this.html = this.html.replaceAll(
         /\n100\.00,00 €<br>/g,
-        '\n100.000,00 €<br>'
+        '\n100.000,00 €<br>',
       )
     } else if (year === 2022) {
       // Ajustes para interpretar correctamente un par de campañas que en el PDF
       // tienen una alineación singular.
       this.html = this.html.replaceAll(
         /Programa Ur Handitan<br>\nUr Handitan es un programa que sirve de<br>(.+)\nObjeto<br>\nTodos los temas del programa están de<br>\n\(descripción\)<br>\n/gs,
-        'Programa Ur Handitan<br>\nObjeto<br>\nUr Handitan es un programa que sirve de<br>\n(descripción)<br>\n$1\nTodos los temas del programa están de<br>\n'
+        'Programa Ur Handitan<br>\nObjeto<br>\nUr Handitan es un programa que sirve de<br>\n(descripción)<br>\n$1\nTodos los temas del programa están de<br>\n',
       )
       this.html = this.html.replaceAll(
         /Colaboración Publicitaria y Producción de<br>\nNombre<br>\n‘Aquí Y Ahora’ en Etb2<br>\n"Aquí y Ahora". El programa de ETB2 es una<br>\n(.+)Objeto<br>\ntemporada de "Aquí y ahora" son:<br>\n\(descripción\)<br>\n/gs,
-        'Colaboración Publicitaria y Producción de<br>\nNombre<br>\n‘Aquí Y Ahora’ en Etb2<br>\nObjeto<br>\n"Aquí y Ahora". El programa de ETB2 es una<br>\n(descripción)<br>\n$1temporada de “Aquí y ahora” son:<br>\n'
+        'Colaboración Publicitaria y Producción de<br>\nNombre<br>\n‘Aquí Y Ahora’ en Etb2<br>\nObjeto<br>\n"Aquí y Ahora". El programa de ETB2 es una<br>\n(descripción)<br>\n$1temporada de “Aquí y ahora” son:<br>\n',
       )
     }
 
@@ -94,7 +94,7 @@ class Interpreter {
 
     if (pageSeparators.length > 1) {
       pages = pages.flatMap((page) =>
-        pageSeparators.slice(1).flatMap((separator) => page.split(separator))
+        pageSeparators.slice(1).flatMap((separator) => page.split(separator)),
       )
     }
 
@@ -181,14 +181,14 @@ class Interpreter {
           100 *
             result.outlets.reduce(
               (previous, current) => (previous += current.euros),
-              0
-            )
+              0,
+            ),
         ) / 100
 
       if (Math.abs(result.euros - sum) >= 1) {
         const difference = Math.round(result.euros - sum)
         console.error(
-          `En ${result.year}, la suma de importes (${sum}) no coincide con el total ${result.euros} por ${difference} €`
+          `En ${result.year}, la suma de importes (${sum}) no coincide con el total ${result.euros} por ${difference} €`,
           // util.inspect(result, { maxArrayLength: null })
         )
 
